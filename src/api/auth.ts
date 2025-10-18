8162bbea6c70775866736d63ab95df2ef4e921cc
Elijah Karori Orito <elijakarori23@gmail.com>
2025-10-18 19:37:51 +0300

    Create auth.ts

---
+++ src/api/auth.ts
@@ -0,0 +1,10 @@
+import axios from "./axios";
+
+export const login = (email: string, password: string) =>
+  axios.post("/auth/login", { username: email, password });
+
+export const register = (payload: any) =>
+  axios.post("/auth/register", payload);
+
+export const getMe = () =>
+  axios.get("/auth/me");
